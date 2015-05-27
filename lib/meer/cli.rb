d03c3337114c20b9bfc3c33ec35cdd32dbbbf69b
philip.monroe <philip.monroe@workday.com>
2015-05-26 17:34:36 -0700

    primitive sorting

--- lib/meer/cli.rb
+++ lib/meer/cli.rb
@@ -1,4 +1,5 @@
 require 'meer/datameer'
+require 'time'
 
 module Meer
   class CLI < ::Thor
@@ -31,10 +32,14 @@ module Meer
     end
     
     desc "table [WORKBOOK-ID] [SHEET_NAME]", "Outputs a nicely formatted table for a given workbook"
+    option :sort, type: :string
     def table(workbook, sheet)
       data = client.workbook_data(workbook, sheet)
       rows = CSV.parse(data, :headers => true).to_a
-      puts Terminal::Table.new headings: rows[0], rows: rows[1..-1]
+      headers = rows.slice!(0)
+      sort!(headers, rows, options[:sort]) if options[:sort]
+      
+      puts Terminal::Table.new(headings: headers, rows: rows)
     end
 
     
@@ -43,5 +48,43 @@ module Meer
     def client user=nil, password=nil
       @client = Datameer.new ENV['DATAMEER_URL'], user, password
     end
+    
+    def sort! headers, rows, sort_str
+      cols = sort_str.split(',').map do |name| 
+        reverse = name[-1] == '-'
+        name    = name[0..-2] if reverse
+        col     = headers.find_index(name) 
+
+        type    = :number if Float(rows.first[col]) rescue false
+        type    = :time   if rows.first[col] =~ /\A\w+ \d{1,2}, \d{4} \d{1,2}:\d{1,2}:\d{1,2} (AM|PM)\z/
+        type  ||= :string
+
+        OpenStruct.new(col: col, type: type, reverse: reverse)
+      end
+      
+      rows.sort_by! do |row| 
+        cols.map do |i| 
+          val = row[i.col]
+          val = val.to_f              if i.type == :number
+          val = Time.parse(val)       if i.type == :time
+          val = ReverseOrder.new(val) if i.reverse
+          val
+        end
+      end
+    end
+    
+    
+    
+    class ReverseOrder 
+      attr_reader :obj
+      def initialize obj
+        @obj = obj
+      end
+  
+      def <=>(other)
+        -(@obj <=> other.obj)
+      end
+    end
   end
 end
+
